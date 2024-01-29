ae41b83c7e650744bb5abdc9ff7fe05ee03bf173
Jabar Asadi <azhovan@outlook.com>
2024-01-29 13:52:12 +0100

    chore: update tests

--- pkg/download/dl_manager_test.go
+++ pkg/download/dl_manager_test.go
@@ -10,7 +10,7 @@ import (
 func TestNewDownloadManager(t *testing.T) {
 	downloader, err := NewDownloader("/tmp/xx/", "https://httpbin.org/range/512")
 	if assert.NoError(t, err) {
-		dlManager := NewDownloadManager(downloader)
+		dlManager := NewDownloadManager(downloader, DefaultRetryPolicy())
 		err = dlManager.StartDownload(context.Background())
 		assert.NoError(t, err)
 	}
